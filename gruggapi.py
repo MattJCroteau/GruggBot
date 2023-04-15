b8dd28eee1fcaa48f88bfa0164087a9a69a88f11
Masen Furer <m_github@0x26.net>
2023-04-14 23:00:05 -0700

    Fix #4: empty prompt redirect to "who grug" page
    submitting the form without a prompt redirects to an info page, and serves
    as a nice way to preview the CSS/style without having to generate a response.
    
    Also links to the original author for a bit of attribution.

--- gruggapi.py
+++ gruggapi.py
@@ -1,5 +1,7 @@
+import typing as t
+
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from gruggbot import Grugg
@@ -21,23 +23,41 @@ ASK_GRUG = """
 <body>
 <center>
 <img id="grug" src="static/grug.png">
-<form action="/is_asked" method="GET">
+<form action="/" method="GET">
+<div class="flex-full-horiz">
 <input type="text" name="q" placeholder="What learns do you want from GruggBot?">
 <input type="submit" value="ask">
+</div>
 </form>
 """
 
 ANSWER = """
 <h1 class="question">%s</h1>
-<p class="answer">%s</p>
+<p>%s</p>
+"""
+
+WHO = """
+  grug brain developer not so smart, but grug brain developer program many long
+  year and learn some things although mostly still confused.
+</p><p>
+  gruggbot is ai language model with embedding trained on
+  <a href="https://grugbrain.dev">grugbrain.dev</a>.
+  now young grugs everywhere can ask question, and grug might be able to help,
+  although maybe get confused more.
 """
 
 
 @app.get("/", response_class=HTMLResponse)
-def ask_grug() -> str:
+def ask_grug(q: t.Optional[str] = None) -> str:
+    if q is not None:
+        if q:
+            return ASK_GRUG + ANSWER % (q, GRUGG.is_asked(q))
+        else:
+            # form submit, but no query entered
+            return RedirectResponse("/who")
     return ASK_GRUG
 
 
-@app.get("/is_asked", response_class=HTMLResponse)
-def is_asked(q: str) -> str:
-    return ASK_GRUG + ANSWER % (q, GRUGG.is_asked(q))
+@app.get("/who", response_class=HTMLResponse)
+def who_grug() -> str:
+  return ASK_GRUG + ANSWER % ("who grug?", WHO)
